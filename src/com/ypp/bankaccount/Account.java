60ab4c7030f88eb23beefdedd23c9b6b3c3b0787
KTGLeiden <koengriffioen@gmail.com>
2020-07-20 15:30:47 +0200

    Code comments
    Nice work! Like the descriptive naming ;)
    See my comments in the changes.

--- src/com/ypp/bankaccount/Account.java
+++ src/com/ypp/bankaccount/Account.java
@@ -62,25 +62,29 @@ public class Account {
 
     public void creditAccount(int amount){
         balance += amount;
-        System.out.println("Credit success");
+        System.out.println("Credit success"); // Print details
     }
 
     public void debitAccount(int amount){
+        // Check is probably not needed - the assignment requests a negative balance at some point in time.
         if(balance < amount){
             System.out.println("Debit not success, not enough balance");
         }
         else {
             balance -= amount;
-            System.out.println("Debit success");
+            System.out.println("Debit success"); // Print details
         }
     }
 
+    // I would rename the account to "other".
     public void transferTo(Account account,int amount) {
+        // If you remove the check, you can support negative balances.
         if(this.balance < amount){
             System.out.println("Transfer not success," + this.accountHolderName + " doesnt have enough balance");
         }
         else {
             this.balance -= amount;
+            // Use the creditaccount function on the other account!!
             account.setBalance(account.getBalance() + amount);
             System.out.println("Transfer success from " + this.accountHolderName + " To " +
                                account.getAccountHolderName());
